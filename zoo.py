bfc55c523af5715d2ffd6edc2c3e550680b0ec44
Charles Jackson <cpjackson572@gmail.com>
2020-01-14 09:53:13 -0600

    added starter code but had to stop exercise for now. WIll continue later

---
+++ zoo.py
@@ -0,0 +1,3 @@
+# Create a tuple named zoo that contains 10 of your favorite animals.
+# Find one of your animals using the tuple.index(value) syntax on the tuple.
+zoo = ("lions", "tigers", "bears", "beavers", "monkeys", "pythons", "frogs", "fish", "elephants", "rhinos")
