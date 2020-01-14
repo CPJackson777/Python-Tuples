4e51a204ec338db469f2613084186bf7e88a74aa
Charles Jackson <cpjackson572@gmail.com>
2020-01-14 11:42:00 -0600

    halfway done with tuples exercise. Breaking for lunch.

--- zoo.py
+++ zoo.py
@@ -1,3 +1,15 @@
 # Create a tuple named zoo that contains 10 of your favorite animals.
-# Find one of your animals using the tuple.index(value) syntax on the tuple.
 zoo = ("lions", "tigers", "bears", "beavers", "monkeys", "pythons", "frogs", "fish", "elephants", "rhinos")
+
+# Find one of your animals using the tuple.index(value) syntax on the tuple.
+print(zoo.index("tigers"))
+
+# Determine if an animal is in your tuple by using value in tuple syntax.
+# animal_to_find = "kangaroo"
+# if animal_to_find in zoo:
+#     # Print that the animal was found
+animalToFind = "bears"
+if animalToFind in zoo:
+    print(f'{animalToFind} are found.')
+
+# You can reverse engineer (unpack) a tuple into another tuple with the following syntax.
